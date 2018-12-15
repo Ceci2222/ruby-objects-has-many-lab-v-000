846f0f906119063ce8e67697295b300de8ac4063
Marie Burns <mcburns2222@gmail.com>
2018-12-15 19:50:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,6 @@
 #require 'pry'
 class Artist 
   attr_accessor :name 
-  #@@all_songs = []
   
   def initialize(name)
     @name = name
@@ -10,24 +9,18 @@ class Artist
   
   def add_song(song)
     @songs << song
-    #@@all_songs << song.name
     song.artist = self 
   end
   
   def add_song_by_name(song)
     song = Song.new(song)
     @songs << song
-    #@@all_songs << song.name
     song.artist = self
   end
   
   def songs
     @songs
   end
-  
-  #def self.song_count
-   # @@all_songs.count
-  #end
   
   def song_count
     Song.@@all.count
