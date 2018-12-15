ce05c92b32631fefaf7ed258883cb4f583cbb9d5
Marie Burns <mcburns2222@gmail.com>
2018-12-15 20:02:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,17 +4,17 @@ class Artist
   
   def initialize(name)
     @name = name
-    @songs = []
+    #@songs = []
   end
   
   def add_song(song)
-    @songs << song
+    #@songs << song
     song.artist = self 
   end
   
   def add_song_by_name(song)
     song = Song.new(song)
-    @songs << song
+    #@songs << song
     song.artist = self
   end
   
